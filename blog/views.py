05722869b1cf096f9a6bcc77995d7995629ba26a
Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>
2018-06-17 23:50:56 +0530

    Inital commit#2

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
